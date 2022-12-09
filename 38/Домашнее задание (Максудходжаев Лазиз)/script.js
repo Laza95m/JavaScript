0645ace8b0ff85a7b8415ff83c6f36ab6c6cf569
Laziz Maksudkhodjaev <laza95.m@gmail.com>
2022-12-09 22:30:33 +0500

    done №38 homework

--- 38/Домашнее задание (Максудходжаев Лазиз)/script.js
+++ 38/Домашнее задание (Максудходжаев Лазиз)/script.js
@@ -1,14 +1,54 @@
-async function getUsersName() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/users");
-  users = await response.json();
+const container = document.querySelector(".container");
 
-  let res = [];
-  await users.forEach((user) => {
-    res.push(user.name);
-    return res;
-  });
+// Функция создания элемента "div", с именами пользователя
+const createElement = (name) => {
+  const div = document.createElement("div");
+  div.classList.add("user_name");
+  div.textContent = name;
+  container.append(div);
+};
 
-  return res;
-}
+const getUsersName = async () => {
+  try {
+    //Получаем данные пользователя с сервера
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
 
-console.log(getUsersName());
+    // Добавляем все имена в функцию "createElement"
+    await users.forEach((user) => {
+      createElement(user.name);
+    });
+
+    // При нажатии на элемент "div" с именами, появляется таблица данных
+    container.addEventListener("click", (e) => {
+      //игнорируем клики вне елемента
+      if (e.target.className === "container") return;
+      const table = document.querySelector("table");
+
+      users.find((user) => {
+        if (e.target.textContent === user.name) {
+          const address = `${user?.address?.city}, ${user?.address?.street}`;
+          const { name, username, email, phone, website } = user;
+          const userData = { name, username, address, email, phone, website };
+          let result = "";
+
+          for (const [key, value] of Object.entries(userData)) {
+            result += `
+            <tr>
+              <td>${key}</td>
+              <td>${value}</td>
+            </tr>`;
+          }
+
+          table.innerHTML = result;
+        }
+      });
+    });
+
+    return users;
+  } catch (error) {
+    throw new Error(`Ошибка в функции "getUsersName" - ${error}`);
+  }
+};
+
+getUsersName();
--- 38/КР/main.js
+++ 38/КР/main.js
@@ -1,13 +1,12 @@
-'use strict'; 
-
+"use strict";
 
 // function getSomeText(callback) {
 //     setTimeout(() => {
 //         callback();
-//     }, 1000); 
+//     }, 1000);
 // }
 
-//Callback HELL 
+//Callback HELL
 // getSomeText(() => {
 //     console.log('Hello');
 //     getSomeText(() => {
@@ -20,22 +19,22 @@
 
 // const result = new Promise((resolve, reject) => {
 //     let a = 5;
-//     // if(typeof a !== "number") reject(new Error(`переменная ${a} не является числом`)); 
+//     // if(typeof a !== "number") reject(new Error(`переменная ${a} не является числом`));
 //     setTimeout(() => {
-//         resolve(a); 
+//         resolve(a);
 //     }, 2000)
 // })
 
 // result
-//     .then(value => { 
-//         return value * 2; 
+//     .then(value => {
+//         return value * 2;
 //     })
 //     .then(value => {
 //         console.log(value);
-//         let b = 999; 
+//         let b = 999;
 //         return new Promise((resolve) => {
 //             setTimeout(() => {
-//                 resolve(b); 
+//                 resolve(b);
 //             }, 2000)
 //         })
 //     })
@@ -43,92 +42,83 @@
 //         console.log(value);
 //     })
 
+// .catch(value => {
+//     throw value;
+// })
 
-    // .catch(value => {
-    //     throw value;
-    // })
-
-
-const test = fetch("https://jsonplaceholder.typicode.com/users")
-
+const test = fetch("https://jsonplaceholder.typicode.com/users");
 
 test
-    .then(res => {
-        if(res.ok && res.status === 200) {
-            return res.json(); 
-        }
-    })
-    .then(data => {
-        console.log(data);
-        createList(data); 
-        getExtraInfo(data); 
-    })
+  .then((res) => {
+    if (res.ok && res.status === 200) {
+      return res.json();
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    createList(data);
+    getExtraInfo(data);
+  });
 
-
-
-const rootEl = document.querySelector('#root'); 
-const tableData = document.querySelector('#tableData'); 
+const rootEl = document.querySelector("#root");
+const tableData = document.querySelector("#tableData");
 
 function getExtraInfo(data) {
-    rootEl.addEventListener('click', e => {
-        if(e.target.id === "root") return; 
-        const id = Number(e.target.parentNode.dataset.id);
-        createTable(id, data)
-    })
+  rootEl.addEventListener("click", (e) => {
+    if (e.target.id === "root") return;
+    const id = Number(e.target.parentNode.dataset.id);
+    createTable(id, data);
+  });
 }
 
 function createTable(id, data) {
-    const tableEl = document.createElement('table'); 
-    tableData.textContent = ''; 
-    for(let user of data) {
-        if(id === user.id) {
-            tableEl.remove()
-            const {email, website, phone, username, name} = user;
-            const userData = {email, website, phone, username, name}; 
-            
-           
-            let result = '';
-            for(let [key, value] of Object.entries(userData)) {
-                // const trEl = document.createElement('tr'); 
-                // const thEl = document.createElement('th'); 
-                // const tdEl = document.createElement('td'); 
-                result += `
+  const tableEl = document.createElement("table");
+  tableData.textContent = "";
+  for (let user of data) {
+    if (id === user.id) {
+      tableEl.remove();
+      const { email, website, phone, username, name } = user;
+      const userData = { email, website, phone, username, name };
+
+      let result = "";
+      for (let [key, value] of Object.entries(userData)) {
+        // const trEl = document.createElement('tr');
+        // const thEl = document.createElement('th');
+        // const tdEl = document.createElement('td');
+        result += `
                     <tr>
                         <th>${key}</th>
                         <td>${value}</td>
                     </tr>
-                `
+                `;
 
-                // thEl.textContent = key; 
-                // tdEl.textContent = value; 
+        // thEl.textContent = key;
+        // tdEl.textContent = value;
 
-                // trEl.append(thEl, tdEl); 
-                // tableEl.append(trEl); 
-            }
-            tableEl.innerHTML = result; 
-        }
+        // trEl.append(thEl, tdEl);
+        // tableEl.append(trEl);
+      }
+      tableEl.innerHTML = result;
     }
-    tableData.innerHTML = tableEl.outerHTML; 
+  }
+  tableData.innerHTML = tableEl.outerHTML;
 }
 
-function createContent({name, id}) {
-    const messageBlock = document.createElement('div'); 
-    const messageText = document.createElement('p'); 
+function createContent({ name, id }) {
+  const messageBlock = document.createElement("div");
+  const messageText = document.createElement("p");
 
-    messageBlock.dataset.id = id; 
+  messageBlock.dataset.id = id;
 
-    messageText.textContent = name; 
-    messageBlock.append(messageText);
-    return messageBlock; 
+  messageText.textContent = name;
+  messageBlock.append(messageText);
+  return messageBlock;
 }
 
 function createList(data) {
-    for(const elem of data) {
-        rootEl.append(createContent(elem)); 
-    }
+  for (const elem of data) {
+    rootEl.append(createContent(elem));
+  }
 }
 
-
-const testEl = document.querySelector('#testBlock'); 
-
-
+const testEl = document.querySelector("#testBlock");
